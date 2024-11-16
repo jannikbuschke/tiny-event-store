f7852e9d986b7dee186ce8facacdd9b2b2bc4790
Jannik <sisosys6@gmail.com>
2024-11-16 08:44:47 +0100

    format

--- src/TinyEventStore.Ef/Core.fs
+++ src/TinyEventStore.Ef/Core.fs
@@ -10,31 +10,9 @@ type DbSideEffect =
   | Delete
   | DoNothing
 
-// let projectToDbCommand (events: EventEnvelope<'id, 'event, 'header> list) =
-//   if (events.Item 0).Version = 1u then
-//     DbSideEffect.Create
-//   else
-//     DbSideEffect.Update
-
-// let mapToDbOperation (db: DbContext) =
-//   function
-//   | DbSideEffect.Create -> db.Add >> ignore
-//   | DbSideEffect.Update -> db.Update >> ignore
-//   | DbSideEffect.Delete -> db.Remove >> ignore
-//   | DbSideEffect.DoNothing ->
-//     // no-op
-//     fun _ -> ()
-
 type IdConverter<'id, 'rawId> = ('id -> 'rawId) * ('rawId -> 'id)
 type Converter<'value, 'dto> = ('value -> 'dto) * ('dto -> 'value)
 
-// [<RequireQualifiedAccess>]
-// type DbSideEffect =
-//   | Create
-//   | Update
-//   | Delete
-//   | DoNothing
-//
 let projectToDbCommand (events: EventEnvelope<'id, 'event, 'header> list) =
   if (events.Item 0).Version = 1u then
     DbSideEffect.Create
